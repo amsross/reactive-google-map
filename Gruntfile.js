2ea034a35e933b71a49d3a619a0ab7a8e1cb3f4d
Anthony Ross <matt@mattross.io>
2015-09-04 17:42:51 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    jshint: {
+      files: ["Gruntfile.js", "src/js/**/*.js", "test/**/*.js"],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    uglify: {
+      app: {
+        files: {
+          "build/js/app.min.js": [
+            "src/js/app.js"
+          ],
+        },
+        options: {
+          sourceMap: true
+        }
+      },
+      vendors: {
+        files: {
+          "build/js/vendors.min.js": [
+            "src/bower_components/lodash/lodash.js",
+            "src/bower_components/jquery/dist/jquery.js",
+            "src/bower_components/highland/dist/highland.js",
+          ]
+        },
+        options: {
+          sourceMap: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: "",
+          open: true
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: [
+          "index.html",
+        ]
+      },
+      jshint: {
+        files: [
+          "<%=jshint.files%>"
+        ],
+        tasks: [
+          "jshint",
+          "uglify:app"
+        ]
+      },
+      vendors: {
+        files: [
+          "bower.json"
+        ],
+        tasks: [
+          "uglify:vendors"
+        ]
+      },
+      livereload: {
+        options: {
+          livereload: 1337
+        },
+        files: [
+          "build/**/*.js",
+          "locations.json",
+          "index.html"
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", [
+    "jshint",
+    "uglify"
+  ]);
+  grunt.registerTask("dev", [
+    "jshint",
+    "uglify",
+    "connect",
+    "watch"
+  ]);
+
+};
---
+++ src/js/app.js
@@ -0,0 +1,88 @@
+(function($, h) {
+
+  $(document).ready(function() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        // instantiate the map a the user"s location
+        buildMap( position.coords.latitude, position.coords.longitude );
+      }, function() {
+        buildMap( 39.916888, -75.070367 );
+      });
+    } else {
+      // instantiate the map a the default location
+      buildMap( 39.916888, -75.070367 );
+    }
+  });
+
+  function buildMap( lat, lng ) {
+
+    var map = new google.maps.Map(document.getElementById("google-map-container"), {
+      center: {
+        lat: lat,
+        lng: lng,
+      },
+      zoom: 15,
+      zoomControl: true,
+      panControl: true
+    });
+
+    map.on = map.addListener;
+    window.markers = {};
+    var change = h( "bounds_changed", map )
+      .debounce( 500 )
+      .flatMap(function() {
+        var latLngBounds = this.getBounds();
+
+        return h( $.getJSON(
+          "locations.json",
+          {
+            top: latLngBounds.getNorthEast().G,
+            right: latLngBounds.getNorthEast().K,
+            bottom: latLngBounds.getSouthWest().G,
+            left: latLngBounds.getSouthWest().K
+          }
+        ));
+      }.bind(map))
+      .each(function( locations ) {
+        var latLngBounds = this.getBounds();
+        _.each( locations, function( location ) {
+          if ( !markers[location.id] && latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
+            var marker = placeMarker( location, map );
+            var infoPane = attachInfoWindow( marker, location, map );
+            window.markers[location.id] = {
+              marker: marker,
+              infoPane: infoPane
+            };
+          } else if ( markers[location.id] && !latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
+            markers[location.id].marker.setMap( null );
+            delete markers[location.id];
+          }
+        });
+      }.bind(map))
+      ;
+
+    function placeMarker( location, map ) {
+      // add a marker for each location
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng( location.lat, location.lng ),
+        icon: location.icon
+      });
+      marker.setMap( map );
+
+      return marker;
+    }
+
+    function attachInfoWindow( marker, location, map ) {
+      // add an infoWindow for each marker
+      var infoPane = new google.maps.InfoWindow({
+        content: location.content
+      });
+      marker.addListener( "click", function() {
+        infoPane.open( map, marker );
+      });
+
+      return infoPane;
+    }
+  }
+
+}(jQuery, highland));
