2cd4ab1154dbe40d314c46e9d2a48aaa12e0528f
Matt Ross <matt@mattross.io>
2015-09-06 22:03:41 +0000

    Introduce jQuery plugin format

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,92 @@
-(function($, h) {
+;(function( $, h, _, window, document, undefined ) {
 
-  $(document).ready(function() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        // instantiate the map a the user"s location
-        buildMap( position.coords.latitude, position.coords.longitude );
-      }, function() {
-        buildMap( 39.916888, -75.070367 );
-      });
-    } else {
-      // instantiate the map a the default location
-      buildMap( 39.916888, -75.070367 );
-    }
-  });
+  var pluginName = "ReactiveGMap",
+    defaults = {
+      mapOpts: {
+        zoom: 15,
+        zoomControl: true,
+        panControl: true
+      }
+    };
 
-  function buildMap( lat, lng ) {
+  function Plugin( element, options ) {
+    this.element = element;
 
-    var map = new google.maps.Map(document.getElementById("google-map-container"), {
-      center: {
-        lat: lat,
-        lng: lng,
-      },
-      zoom: 15,
-      zoomControl: true,
-      panControl: true
-    });
+    this.options = $.extend( {}, defaults, options );
 
-    map.on = map.addListener;
-    window.markers = {};
-    var change = h( "bounds_changed", map )
-      .debounce( 500 )
-      .flatMap(function() {
-        var latLngBounds = this.getBounds();
+    this._defaults = defaults;
+    this._name = pluginName;
 
-        return h( $.getJSON(
-          "locations.json",
-          {
-            top: latLngBounds.getNorthEast().G,
-            right: latLngBounds.getNorthEast().K,
-            bottom: latLngBounds.getSouthWest().G,
-            left: latLngBounds.getSouthWest().K
-          }
-        ));
-      }.bind(map))
-      .each(function( locations ) {
-        var latLngBounds = this.getBounds();
-        _.each( locations, function( location ) {
-          if ( !markers[location.id] && latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
-            var marker = placeMarker( location, map );
-            var infoPane = attachInfoWindow( marker, location, map );
-            window.markers[location.id] = {
-              marker: marker,
-              infoPane: infoPane
-            };
-          } else if ( markers[location.id] && !latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
-            markers[location.id].marker.setMap( null );
-            delete markers[location.id];
-          }
-        });
-      }.bind(map))
-      ;
+    this.init();
+  }
 
-    function placeMarker( location, map ) {
+  Plugin.prototype = {
+
+    markers: {},
+
+    init: function() {
+
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          // instantiate the map a the user"s location
+          this.buildMap( position.coords.latitude, position.coords.longitude );
+        }.bind(this), function() {
+          this.buildMap( this.options.lat, this.options.lng );
+        }.bind(this));
+      } else {
+        // instantiate the map a the default location
+        this.buildMap( this.options.lat, this.options.lng );
+      }
+    },
+
+    buildMap: function( lat, lng ) {
+
+      var mapOpts = $.extend( {
+        center: {
+          lat: lat,
+          lng: lng
+        }
+      }, this.options.mapOpts );
+
+      this.map = new google.maps.Map( this.element, mapOpts);
+
+      this.map.on = this.map.addListener;
+
+      var change = h( "bounds_changed", this.map )
+        .debounce( 500 )
+        .flatMap(function() {
+          var latLngBounds = this.map.getBounds();
+
+          return h( $.getJSON(
+            "locations.json",
+            {
+              top: latLngBounds.getNorthEast().G,
+              right: latLngBounds.getNorthEast().K,
+              bottom: latLngBounds.getSouthWest().G,
+              left: latLngBounds.getSouthWest().K
+            }
+          ));
+        }.bind(this))
+        .each(function( locations ) {
+          var latLngBounds = this.map.getBounds();
+          _.each( locations, function( location ) {
+            if ( !this.markers[location.id] && latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
+              var marker = this.placeMarker( location, this.map );
+              var infoPane = this.attachInfoWindow( marker, location, this.map );
+              this.markers[location.id] = {
+                marker: marker,
+                infoPane: infoPane
+              };
+            } else if ( this.markers[location.id] && !latLngBounds.contains( new google.maps.LatLng( location.lat, location.lng ) ) ) {
+              this.markers[location.id].marker.setMap( null );
+              delete this.markers[location.id];
+            }
+          }.bind(this));
+        }.bind(this))
+        ;
+    },
+
+    placeMarker: function( location, map ) {
       // add a marker for each location
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng( location.lat, location.lng ),
@@ -70,9 +95,9 @@
       marker.setMap( map );
 
       return marker;
-    }
+    },
 
-    function attachInfoWindow( marker, location, map ) {
+    attachInfoWindow: function( marker, location, map ) {
       // add an infoWindow for each marker
       var infoPane = new google.maps.InfoWindow({
         content: location.content
@@ -83,6 +108,16 @@
 
       return infoPane;
     }
-  }
+  };
 
-}(jQuery, highland));
+  $.fn[pluginName] = function( options ) {
+    return this.each(function() {
+      if ( !$.data( this, "plugin_" + pluginName ) ) {
+        $.data(
+          this, "plugin_" + pluginName,
+          new Plugin( this, options )
+          );
+      }
+    });
+  };
+})( jQuery, highland, _, window, document );
